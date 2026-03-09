f1ac090b28de4fe698feb709179270c7ddbc15cb
FleetingLore <Macro__9@outlook.com>
2026-03-09 18:29:36 +0800

    流程分离

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,73 +1,23 @@
+use std::fs;
+use std::path::Path;
 use html::LoreHtml;
+use parser::file::file_from_text_to_lore;
+use parser::file::file_from_lore_to_html;
 
 fn main() {
-    use parser::file::file_from_text_to_lore;
-    use parser::file::file_from_lore_to_html;
-    let example = "
-+ tests
-  baidu | https://www.baidu.com
-  ok
-  + 二级领域
-    + 三级领域
-      aaa
-      67890
-      + 四级领域
-        aaa".to_string();
+    let example_lore = Path::new("test/lore/local.lore");
+    let example_lore = fs::read_to_string(example_lore).unwrap().to_string();
+    let example_lore = file_from_text_to_lore(example_lore);
+    let example_lore = file_from_lore_to_html(example_lore);
 
-    let style = r##"<style>
-:root {
-    --bg: #141516;
-    --text: #E6EDF0;
-    --text-muted: #c4d9ff;
-    --primary: #68c7c7;
-    --error: #E67E7E;
-}
+    let style = Path::new("test/css/style.css");
+    let style = fs::read_to_string(style).unwrap().to_string();
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-body {
-    background: var(--bg);
-    color: var(--text);
-    font-family: sans-serif;
-    line-height: 1.6;
-    padding: 20px;
-}
-
-br {
-    height: 1rem;
-}
-
-p {
-    color: var(--text);
-    margin-bottom: 1.2rem;
-}
-
-strong {
-    color: var(--primary);
-    font-size: 1.2rem;
-}
-
-a {
-    color: var(--text-muted);
-    text-decoration: none;
-}
-
-a:hover {
-    opacity: 0.8;
-}
-</style>"##;
-    
-    let lore = file_from_text_to_lore(example);
-    let lore_html_lines = file_from_lore_to_html(lore);
-    let lore_html = LoreHtml::new(
+    let target: String = LoreHtml::new(
         "Example".to_string(),
         style.to_string(),
-        lore_html_lines,
-    );
-    let target: String = lore_html.into();
-    println!("{}", target);
+        example_lore
+    ).into();
+
+    fs::write("test/output/example.html", target).expect("File writing error");
 }
